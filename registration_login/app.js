1870c90f8ee34e38b7b1c4ee49b70dbc8577d305
Julian Chih <chih.julian@gmail.com>
2025-05-05 11:26:01 -0700

    changing the code

--- registration_login/app.js
+++ registration_login/app.js
@@ -3,24 +3,20 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
-
 const userRoutes = require('./routes/users');
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+connectionLimit: 10,
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE
 });
-
 app.set('pool', pool);
 app.use('/users', userRoutes);
-
-const PORT = 8232;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
